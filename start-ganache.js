3393b8f594578157b40c0afea49440fc44fdc7e1
cliffhall <github@futurescale.com>
2019-12-04 18:55:26 -0500

    Push new art to chain * Replaced the database trait dump with the latest delivered art * Pushed to chain with no new reverts * Modified start-ganache.js to show only eth_call, eth_sendTransaction, and eth_balance

--- start-ganache.js
+++ start-ganache.js
@@ -5,8 +5,12 @@ const ganache = spawn('npx', ['ganache-cli', '-p 7545', '-d', `-m '${keys.mnemon
 
 ganache.stdout.on('data', (data) => {
     let trimmed = String(data).trim();
-    let regex = /eth_getFilterChanges/gi;
-    let filtered = trimmed.replace(regex, '');
+    let regex = /eth_getFilterChanges|eth_getBlockByNumber|eth_blockNumber|net_version|eth_accounts|eth_getCode|eth_getTransactionReceipt|eth_estimateGas/gi;
+    let filtered = trimmed.replace(regex, '').trim();
+    regex = /eth_sendTransaction/gi;
+    filtered = filtered.replace(regex, '\neth_sendTransaction');
+    regex = /eth_call/gi;
+    filtered = filtered.replace(regex, '\neth_call');
     if (filtered.length > 0) console.log(filtered);
 });
 
